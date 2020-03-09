8746054a16a921db6ce999f75da9da065b8d3873
Gogo <gogo81745.moe@gmail.com>
2020-03-09 11:53:22 +0800

    init project1

---
+++ Project1/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    kotlin("jvm") version "1.3.70"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+
+    implementation("io.vertx:vertx-core:4.0.0-milestone4")
+    implementation("io.vertx:vertx-lang-kotlin:4.0.0-milestone4")
+    implementation("io.vertx:vertx-lang-kotlin-coroutines:4.0.0-milestone4")
+
+    implementation("io.vertx:vertx-jdbc-client:4.0.0-milestone4")
+
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
