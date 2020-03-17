8b0ee62bc215f0d1479c1caa0f32edf6638b1ec2
Gogo <gogo81745.moe@gmail.com>
2020-03-17 17:45:38 +0800

    update assignment02

--- Assignment02/q3.sql
+++ Assignment02/q3.sql
@@ -2,4 +2,4 @@ WITH counts AS (SELECT country, count(*) AS count
                 FROM movies
                 WHERE year_released BETWEEN 1970 AND 1979
                 GROUP BY country)
-SELECT round(100 * (SELECT count FROM counts WHERE country = 'us') / (SELECT sum(count) FROM counts), 2) AS us_percent;
+SELECT round(100.0 * (SELECT count FROM counts WHERE country = 'us') / (SELECT sum(count) FROM counts), 2) AS us_percent;
